9639c93ee8d76bb95788238469f7769f49e9aba3
Arruda Junior <juniorarruda15@gmail.com>
2024-05-16 08:28:24 -0300

    Previnindo bug de um valor no campo Do número maior que o valor informado no campo Até o número

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+        alert('O número incial é maior que o número final, digite novamente:')
+        return;
+    }
+
     let sorteados = [];
     let numero;
 
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
+
 
         while (sorteados.includes(numero)) {
             numero = obterNumeroAleatorio(de, ate);
